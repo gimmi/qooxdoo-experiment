a546af2482e8034c893988ff99ae52a7edc42b27
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-07-27 10:12:16 +0200

    Refactoring on EditTaskComposite

--- src/Trackr/source/class/trackr/view/EditTaskComposite.js
+++ src/Trackr/source/class/trackr/view/EditTaskComposite.js
@@ -1,33 +1,42 @@
 ﻿qx.Class.define("trackr.view.EditTaskComposite", {
 	extend: qx.ui.container.Composite,
 
-	construct: function (taskId) {
+	construct: function(taskId) {
 		this.base(arguments);
 
 		this._taskId = taskId;
 
 		var form = this.__createForm();
 
-		var store = new qx.data.store.Json("/rpc?class=Trackr.TaskRepository&method=Get", {
-			configureRequest: function (req) {
-				req.setMethod("PUT");
-				req.setRequestHeaders({ "Content-Type": "application/json" });
-				req.setRequestData(qx.lang.Json.stringify({ taskId: taskId }));
-			}
-		});
-
-		var controller = new qx.data.controller.Form(null, form);
-
-		store.bind("model", controller, "model");
+		this._controller = new qx.data.controller.Form(null, form);
 
 		this._setLayout(new qx.ui.layout.Canvas());
 		this._add(new qx.ui.form.renderer.Single(form), { left: 10, top: 10 });
+
+		this.__loadTask();
 	},
 
 	members: {
 		_taskId: null,
+		_store: null,
+		_controller: null,
 
-		__createForm: function () {
+		__loadTask: function() {
+			if (this._store) {
+				this._store.reload();
+				return;
+			}
+			this._store = new qx.data.store.Json("/rpc?class=Trackr.TaskRepository&method=Get", {
+				configureRequest: qx.lang.Function.bind(function(req) {
+					req.setMethod("PUT");
+					req.setRequestHeaders({ "Content-Type": "application/json" });
+					req.setRequestData(qx.lang.Json.stringify({ taskId: this._taskId }));
+				}, this)
+			});
+			this._store.bind("model", this._controller, "model");
+		},
+
+		__createForm: function() {
 			var form = new qx.ui.form.Form();
 
 			var id = new qx.ui.form.TextField();
