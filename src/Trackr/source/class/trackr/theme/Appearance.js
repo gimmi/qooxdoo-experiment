d4f89f63080456c7daacc9206c907da8bc9ac311
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-07-29 19:43:52 +0200

    Removed unused appearances

--- src/Trackr/source/class/trackr/theme/Appearance.js
+++ src/Trackr/source/class/trackr/theme/Appearance.js
@@ -1,7 +1,3 @@
 qx.Theme.define("trackr.theme.Appearance", {
-	extend      : qx.theme.modern.Appearance,
-	appearances : {
-		"search-task-widget": "widget",
-		"search-task-widget/table": "table"
-	}
+	extend      : qx.theme.modern.Appearance
 });
