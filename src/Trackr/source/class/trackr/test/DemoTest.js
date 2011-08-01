e72a3874620afc8c4392e5308f9680cee79e20fb
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-08-01 18:01:56 +0200

    Added test for qooxdoo objects to JSON serialization/deserialization

--- src/Trackr/source/class/trackr/test/DemoTest.js
+++ src/Trackr/source/class/trackr/test/DemoTest.js
@@ -1,26 +1,26 @@
 qx.Class.define("trackr.test.DemoTest", {
-	extend : qx.dev.unit.TestCase,
+	extend: qx.dev.unit.TestCase,
 
-	members : {
-		testSimple : function() {
+	members: {
+		testSimple: function () {
 			this.assertEquals(4, 3 + 1, "This should never fail!");
 			this.assertFalse(false, "Can false be true?!");
 		},
 
-		testAdvanced : function() {
+		testAdvanced: function () {
 			var a = 3;
 			var b = a;
 			this.assertIdentical(a, b, "A rose by any other name is still a rose");
 			this.assertInRange(3, 1, 10, "You must be kidding, 3 can never be outside [1,10]!");
 		},
 
-		"test: should do json encoded request with qx.io.request": function() {
+		"test: should do json encoded request with qx.io.request": function () {
 			var req = new qx.io.request.Xhr("/rpc?class=Trackr.TaskInfoRepository&method=GetAll", "PUT");
-			req.setRequestHeaders({ "Content-Type" : "application/json" });
-			req.setRequestData(qx.lang.Json.stringify({ "par" : "Hello world" }));
+			req.setRequestHeaders({ "Content-Type": "application/json" });
+			req.setRequestData(qx.lang.Json.stringify({ "par": "Hello world" }));
 
-			req.addListener("success", function(e) {
-				this.resume(function() {
+			req.addListener("success", function (e) {
+				this.resume(function () {
 					this.assertEquals(200, e.getTarget().getStatus());
 				}, this);
 			},
@@ -31,12 +31,12 @@ qx.Class.define("trackr.test.DemoTest", {
 			this.wait(1000);
 		},
 
-		"test: should do json encoded request with qx.io.remote": function() {
+		"test: should do json encoded request with qx.io.remote": function () {
 			var req = new qx.io.remote.Request("/rpc?class=Trackr.TaskInfoRepository&method=GetAll", "POST", "application/json");
 			req.setRequestHeader("Content-Type", "application/json");
-			req.setData(qx.lang.Json.stringify({ "par" : "Hello world" }));
+			req.setData(qx.lang.Json.stringify({ "par": "Hello world" }));
 
-			req.addListener("completed", function(e) {
+			req.addListener("completed", function (e) {
 				this.resume(function () {
 					var response = e.getContent();
 					//this.assertEquals(200, e.getTarget().getStatus());
@@ -47,6 +47,34 @@ qx.Class.define("trackr.test.DemoTest", {
 			req.send();
 
 			this.wait(1000);
+		},
+
+		"test: should create nested objects": function () {
+			var model = qx.data.marshal.Json.createModel({
+				id: 1,
+				details: [{
+					id: 2
+				}, {
+					id: 3
+				}]
+			}, true);
+			
+			this.assertEquals(2, model.getDetails().length);
+		},
+
+		"test: should serialize nested objects": function () {
+			var model = qx.data.marshal.Json.createModel({
+				id: 1,
+				details: [{
+					id: 2
+				}, {
+					id: 3
+				}]
+			}, true);
+
+			var json = qx.lang.Json.stringify(qx.util.Serializer.toNativeObject(model));
+			
+			this.assertEquals('{"id":1,"details":[{"id":2},{"id":3}]}', json);
 		}
 	}
 });
