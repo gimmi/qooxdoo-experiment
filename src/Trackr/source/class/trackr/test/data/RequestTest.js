9fd795b3e224b4c238d839e316c21a91f6efde16
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-08-05 14:05:21 +0200

    Added trackr.data.Request tests

--- src/Trackr/source/class/trackr/test/data/RequestTest.js
+++ src/Trackr/source/class/trackr/test/data/RequestTest.js
@@ -15,11 +15,55 @@
 			this.getSandbox().restore();
 		},
 
-		"test: should notify and set response received from server": function () {
-			this.getServer().respondWith("PUT", "/rpc?class=Class&method=Method", [ 200,  { "Content-Type": "application/json" }, '[{"id":12,"comment":"Hey there"}]']);
-			this.__target.send({});
+		"test: should invoke the expected url": function () {
+			var method, url;
+			this.getServer().respondWith(/.*/, function (xhr) {
+				url = xhr.url;
+				method = xhr.method;
+				xhr.respond();
+			});
+
+			this.__target.send(null);
+
 			this.getServer().respond();
-			this.assertJsonEquals([{ id: 12, comment: "Hey there" }], this.__target.getResponse());
+			this.assertEquals("PUT", method);
+			this.assertEquals("/rpc?class=Class&method=Method", url);
+		},
+
+		"test: should set response": function () {
+			this.getServer().respondWith([200, { "Content-Type": "application/json" }, '123']);
+
+			this.__target.send(null);
+
+			this.getServer().respond();
+			this.assertEquals(123, this.__target.getResponse());
+		},
+
+		"test: should call callback": function () {
+			this.getServer().respondWith([200, { "Content-Type": "application/json" }, '123']);
+			var callback = this.spy();
+			var scope = {};
+
+			this.__target.send(null, callback, scope);
+
+			this.getServer().respond();
+			this.assert(1, callback.callCount);
+			this.assertIdentical(scope, callback.getCall(0).thisValue);
+			this.assert(callback.getCall(0).calledWithExactly(123));
+		},
+
+		"test: should convert response": function () {
+			this.getServer().respondWith([200, { "Content-Type": "application/json" }, '123']);
+			this.__target.setResponseConverter(function (value) {
+				return "Converted " + value;
+			});
+			var callback = this.spy();
+
+			this.__target.send(null, callback);
+
+			this.getServer().respond();
+			this.assertEquals("Converted 123", this.__target.getResponse());
+			this.assert(callback.calledWith("Converted 123"));
 		}
 	}
 });
