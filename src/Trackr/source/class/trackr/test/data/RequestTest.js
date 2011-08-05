5def41aacf8e33558f65060f6c173b37b7a8bb20
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-08-05 12:00:21 +0200

    Added trackr.data.Request test

---
+++ src/Trackr/source/class/trackr/test/data/RequestTest.js
@@ -0,0 +1,25 @@
+﻿qx.Class.define("trackr.test.data.RequestTest", {
+	extend: qx.dev.unit.TestCase,
+
+	include: [qx.dev.unit.MMock],
+
+	members: {
+		__target: null,
+
+		setUp: function () {
+			this.useFakeServer();
+			this.__target = new trackr.data.Request("Class", "Method");
+		},
+
+		tearDown: function () {
+			this.getSandbox().restore();
+		},
+
+		"test: should notify and set response received from server": function () {
+			this.getServer().respondWith("PUT", "/rpc?class=Class&method=Method", [ 200,  { "Content-Type": "application/json" }, '[{"id":12,"comment":"Hey there"}]']);
+			this.__target.send({});
+			this.getServer().respond();
+			this.assertJsonEquals([{ id: 12, comment: "Hey there" }], this.__target.getResponse());
+		}
+	}
+});
